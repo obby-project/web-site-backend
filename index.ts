72db3dc5ce46947dd6e23b39e8325ff6221410ed
egorguscha <forcejmage@gmail.com>
2023-02-20 23:23:27 +0300

    foo

--- index.ts
+++ index.ts
@@ -18,61 +18,66 @@ AWS.config.credentials = credentials;
 AWS.config.update({
   region: REGION,
 });
-const app = express();
-app.use(express.json());
-app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-const db = new DynamoDB.DocumentClient({
-  convertEmptyValues: true,
-  endpoint: "https://dynamodb.eu-central-1.amazonaws.com",
-});
+// const db = new DynamoDB.DocumentClient({
+//   convertEmptyValues: true,
+//   endpoint: "https://dynamodb.eu-central-1.amazonaws.com",
+// });
 
-const findItem = async (id: string) => {
-  const params = {
-    TableName: TABLE_NAME,
-    Key: {
-      id,
-    },
-  };
+// const findItem = async (id: string) => {
+//   const params = {
+//     TableName: TABLE_NAME,
+//     Key: {
+//       id,
+//     },
+//   };
 
-  return await db.get(params).promise();
-};
-const create = async (id: string, data: any) => {
-  const params: PutItemInput = {
-    TableName: TABLE_NAME,
-    Item: {
-      id,
-      ...data,
-    },
-  };
+//   return await db.get(params).promise();
+// };
+// const create = async (id: string, data: any) => {
+//   const params: PutItemInput = {
+//     TableName: TABLE_NAME,
+//     Item: {
+//       id,
+//       ...data,
+//     },
+//   };
 
-  await db.put(params).promise();
+//   await db.put(params).promise();
 
-  return params.Item;
-};
+//   return params.Item;
+// };
 
-app.post("/api/v1/waitlist", async (req, res) => {
-  const data = req.body;
+// app.post("/api/v1/waitlist", async (req, res) => {
+//   const data = req.body;
 
-  const foundItem = await findItem(data.twitter);
+//   const foundItem = await findItem(data.twitter);
 
-  if (foundItem.Item) {
-    res.json({ code: 409, message: "Item already exists" });
+//   if (foundItem.Item) {
+//     res.json({ code: 409, message: "Item already exists" });
 
-    return;
-  }
-  const created = await create(data.twitter, data);
+//     return;
+//   }
+//   const created = await create(data.twitter, data);
 
-  res.json(created);
-});
-app.get("/", async (req, res, next) => {
-  res.status(200).send("Hello World!!");
-});
+//   res.json(created);
+// });
+// app.get("/", async (req, res, next) => {
+//   res.status(200).send("Hello World!!");
+// });
 
 if (process.env.ENVIRONMENT === "production") {
-  exports.handler = serverless(app, { provider: "aws" });
+  exports.handler = () => {
+    return {
+      statusCode: 200,
+      body: "Go ahead without me.",
+    };
+  };
 } else {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}.`);
-  });
+  // app.listen(PORT, () => {
+  //   console.log(`Server is listening on port ${PORT}.`);
+  // });
 }
